7f1834e55e1eff0e4ee6a05a496267d3a1fef153
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-03-22 23:51:38 +0700

    Add delete method

--- routes/booking.js
+++ routes/booking.js
@@ -127,4 +127,24 @@ router.put('/:id', (req, res, next) => {
     });
 });
 
+// DELETE data
+router.delete('/:id', (req, res, next) => {
+  Booking.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(() => {
+      res.send({
+        message: 'Delete data successfully.'
+      });
+    })
+    .catch(err => {
+      res.send({
+        message: 'Unable create data',
+        err
+      });
+    });
+});
+
 module.exports = router;
